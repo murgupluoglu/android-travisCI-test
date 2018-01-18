a84b22318aeae30973aae930bf164b8426ed6a2c
Mustafa Urgupluoglu <mustafa.urgupluoglu@cellsigma.com>
2018-01-18 11:54:07 +0300

    run test

--- app/src/main/java/com/mustafaurgupluoglu/traviscitest/MainActivity.java
+++ app/src/main/java/com/mustafaurgupluoglu/traviscitest/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//r
+        setContentView(R.layout.activity_main);//rt
     }
 }
